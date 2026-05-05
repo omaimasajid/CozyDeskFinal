dc235cdd45ff419b7189b31250965653df1be3a7
Areeba Znb <reebuznb@gmail.com>
2026-05-05 21:06:37 +0500

    hohohoh

--- CozyDeskFinal/MusicPlayer.h
+++ CozyDeskFinal/MusicPlayer.h
@@ -7,6 +7,23 @@
 class MusicPlayer : public QObject
 {
 	Q_OBJECT
-
+public:
+	explicit MusicPlayer(QObject* ptr = nullptr);
+	void loadfiles(QStringList file);
+	void play();
+	void pause();
+	void nextSong();
+	void prevSong();
+	void setVolume(int v);
+	void setMusicSlider(int v);
+signals:
+	void positionChange(qint64 p);
+	void durationChange(qint64 d);
+	void songChange(QString name);
+private:
+	QMediaPlayer* mp;
+	QAudioOutput* ao;
+	QStringList playlist;
+	int songIndex;
 };
 
